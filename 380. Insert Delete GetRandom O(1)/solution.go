6d6a7b279bdc28bc7439d3160bbdf037d7de65f5
Simon <simonm96@gmx.de>
2024-09-30 13:38:56 +0200

    some progress

--- 380. Insert Delete GetRandom O(1)/solution.go
+++ 380. Insert Delete GetRandom O(1)/solution.go
@@ -1,20 +1,32 @@
 package main
 
+import "math/rand"
+
 func main() {
 
 }
 
 type RandomizedSet struct {
-	rset map[int]struct{}
+	rset  map[int]int // maps value to index
+	rlist map[int]int // maps index to value
+	size  int         // int of length of entries
 }
 
+// Constructor initializes a new instance of RandomizedSet
 func Constructor() RandomizedSet {
+	return RandomizedSet{
+		rset:  make(map[int]int),
+		rlist: make(map[int]int),
+		size:  0,
+	}
 }
 
 func (this *RandomizedSet) Insert(val int) bool {
 	_, exists := this.rset[val]
 	if !exists {
-		this.rset[val] = struct{}{}
+		this.rset[val] = this.size
+		this.rlist[this.size] = val
+		this.size++
 		return true
 	} else {
 		return false
@@ -22,9 +34,11 @@ func (this *RandomizedSet) Insert(val int) bool {
 }
 
 func (this *RandomizedSet) Remove(val int) bool {
-	_, exists := this.rset[val]
+	idx, exists := this.rset[val]
 	if !exists {
 		delete(this.rset, val)
+		delete(this.rlist, idx)
+		this.size--
 		return true
 	} else {
 		return false
@@ -32,7 +46,8 @@ func (this *RandomizedSet) Remove(val int) bool {
 }
 
 func (this *RandomizedSet) GetRandom() int {
-
+	val, _ := this.rlist[rand.Intn(this.size)]
+	return val
 }
 
 /**
--- main.go
+++ main.go
@@ -1,37 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println(plusOne([]int{9, 9}))
-}
 
-func plusOne(digits []int) []int {
-
-	i := len(digits) - 1
-	carry := 1
-
-	for carry != 0 {
-		if i == 0 && digits[i] == 9 {
-			digits[i] = 0
-			digits = append(digits, 0)
-			for j := len(digits) - 1; j >= 0; j-- {
-				if j != 0 {
-					digits[j] = digits[j-1]
-				} else {
-					digits[j] = 1
-				}
-			}
-		} else {
-			if digits[i] == 9 {
-				digits[i] = 0
-				i--
-				carry++
-			} else {
-				digits[i] = digits[i] + 1
-			}
-		}
-		carry--
-	}
-	return digits
 }
